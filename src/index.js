c942a86745caa4931c5b737c5777e801324dbe6c
amansarosh <amansarosh@gmail.com>
2021-01-03 13:28:25 -0800

    Now loops over books array.

--- src/index.js
+++ src/index.js
@@ -3,47 +3,41 @@ import ReactDom from 'react-dom'; // Need this import for displaying
 import './index.css' // css import
 
 // setup variables
-  const firstBook = {
+const books = [  // Every item in array is an object
+{   id: 1,
     img: "https://images-na.ssl-images-amazon.com/images/I/41L5qgUW2fL._SX327_BO1,204,203,200_.jpg",
     title: 'A Promised Land',
     author: 'Barack Obama'
   }
-
-  const secondBook = {
+,
+ {
+    id: 2,
     img: "https://images-na.ssl-images-amazon.com/images/I/51Tlm0GZTXL._SX329_BO1,204,203,200_.jpg",
     title: 'Atomic Habits',
     author: 'James Clear'
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/41czXAfMbDL._SX324_BO1,204,203,200_.jpg",
+    title: 'Upstream: The Quest to Solve Problems Before They Happen',
+    author: 'Dan Heath'
   }
+,
+];
+
 
 function BookList() {
-  return( 
-  <section className="booklist">
-  <Book
-  img={firstBook.img}
-  title={firstBook.title}
-  author={firstBook.author}
-  >
-
-<p>Lorem ipsum dolor sit amet consectetur 
-adipisicing elit. Accusamus sequi vel quod,
-veniam distinctio ex reiciendis totam praesentium 
-ullam assumenda!
-</p>
-
-  </Book>
-
-  <Book 
-  img = {secondBook.img}
-  title = {secondBook.title}
-  author = {secondBook.author}
-  />
-
-  </section>
-  )};
+  return( <section className="booklist"> {books.map((book) => { // Now using the .map function we loop over the whole Book component.
+    return (
+  <Book key={book.id} {...book}></Book>
+    );
+  })} </section>
+  )
+};
 
 const Book = (props) => {
-
-  const { img, title, author, children } = props
+const { img, title, author } = props
+console.log(props)
 
   return (
   <article className='book'>
@@ -51,7 +45,6 @@ const Book = (props) => {
     <img src={img} alt="A Promised Land"/>
     <h1>{title}</h1>
     <h4>{author}</h4>
-    {children}
 
   </article>
   )}
